d691fef47ffab77707c3c44667634be0fd901fb5
faithyiyo <1297232812@qq.com>
2019-09-07 17:47:28 +0800

    first commit

---
+++ test1.py
@@ -0,0 +1,11 @@
+从个github下载：
+git clone git@github.com:tangxingwang/my_github.git
+
+上传到github：
+cd my_github
+touch test.py
+git add test.py
+git status
+git commit -m"first commit"
+git push origin master
+
